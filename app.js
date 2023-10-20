328db890bcd0a4710ab3a1942b2cdc762142fcdc
Akanksha3231 <akankshakalbhor3231@gmail.com>
2023-10-20 14:16:50 +0530

    profile section added

--- app.js
+++ app.js
@@ -42,3 +42,38 @@ function checkPassword(){
         message.textContent = "";
     }
 }
+
+
+const fileInput = document.getElementById('file-input');
+        const avatar = document.getElementById('avatar');
+    
+        fileInput.addEventListener('change', function () {
+            const file = fileInput.files[0];
+    
+            if (file) {
+                const reader = new FileReader();
+    
+                reader.onload = function (e) {
+                    avatar.src = e.target.result;
+                };
+    
+                reader.readAsDataURL(file);
+            }
+        });
+    
+        function validateForm() {
+            // Get the user input values
+            const fullName = document.getElementById('full-name').value;
+            const dob = document.getElementById('dob').value;
+            const mobile = document.getElementById('mobile').value;
+            const email = document.getElementById('email').value;
+    
+            // Build the URL with query parameters
+            const profileURL = `profile.html?full-name=${fullName}&dob=${dob}&mobile=${mobile}&email=${email}`;
+    
+            // Redirect to the profile.html page with the query parameters
+            window.location.href = profileURL;
+    
+            // Prevent the form from submitting
+            return false;
+        }
