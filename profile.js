5ee3e1c1c9dbc574741898955b68ce017f7fecaf
Shivani <shivanijoldapke0@gmail.com>
2023-10-25 10:59:04 +0530

    home page updated

--- profile.js
+++ profile.js
@@ -1,5 +1,15 @@
 const fileInput = document.getElementById('file-input');
         const avatar = document.getElementById('avatar');
+
+        let sidebar = document.querySelector(".sidebar");
+        let sidebarBtn = document.querySelector(".sidebarBtn");
+        sidebarBtn.onclick = function () {
+            sidebar.classList.toggle("active");
+            if (sidebar.classList.contains("active")) {
+                sidebarBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+            } else
+                sidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+        }
     
         fileInput.addEventListener('change', function () {
             const file = fileInput.files[0];
@@ -31,3 +41,5 @@ const fileInput = document.getElementById('file-input');
             // Prevent the form from submitting
             return false;
         }
+
+       
