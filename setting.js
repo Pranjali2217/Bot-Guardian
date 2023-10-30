8d95502deed1fa131cf1735e66a4f7e3cc775107
Pranjali2217 <pranjalidalvi2001@gmail.com>
2023-10-30 10:31:17 +0530

    setting updated

--- setting.js
+++ setting.js
@@ -24,3 +24,29 @@ const fileInput = document.getElementById('file-input');
                 reader.readAsDataURL(file);
             }
         });
+
+        
+        // JavaScript functions for theme change, account deletion, and password reset
+        function changeTheme() {
+            const themeSelect = document.getElementById("themeSelect");
+            const selectedTheme = themeSelect.value;
+
+            if (selectedTheme === "dark") {
+                document.body.className = "dark-theme";
+            } else {
+                document.body.className = "light-theme";
+            }
+        }
+
+        function deleteAccount() {
+            const confirmDelete = confirm("Are you sure you want to delete your account? This action cannot be undone.");
+            if (confirmDelete) {
+                // You would typically make an AJAX request to the server to delete the account.
+                alert("Account deleted!");
+            }
+        }
+
+        function resetPassword() {
+            // You would typically navigate to a password reset page.
+            alert("Password reset link sent to your email.");
+        }           
