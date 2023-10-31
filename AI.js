6d6d4de98da6601b57e1ec492ad0d88f102175ab
Akanksha3231 <akankshakalbhor3231@gmail.com>
2023-10-31 23:39:28 +0530

    few change

--- AI.js
+++ AI.js
@@ -47,7 +47,7 @@ const getChatResponse = async (incomingChatDiv) => {
         body: JSON.stringify({
             model: "text-davinci-003",
             prompt: userText,
-            max_tokens: 2048,
+            max_tokens: 2048, 
             temperature: 0.2,
             n: 1,
             stop: null
